e3a1f2999cac5935b3f42f751b976d0d0acfc2bd
tanermen <532410116@qq.com>
2018-07-10 17:15:38 +0800

    完善配置

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import Home from './Home'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Home/>
       </div>
     );
   }
