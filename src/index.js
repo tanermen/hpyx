ed85ecb78931314c3ec40f8463a54c0ee1cf0e85
tanermen <532410116@qq.com>
2018-07-10 16:26:41 +0800

    配置全局样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// 全局引入rem
+import './modules/rem.js'
+
+//全局样式引入
+import './stylesheets/main.scss'
+
+// 全局配置
+import './modules/config.js'
+
+// 全局引入Post Get 方法
+import './modules/request'
+
+// 引入swiper
+import 'swiper/dist/css/swiper.min.css'
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
